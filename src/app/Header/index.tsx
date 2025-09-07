7765b9f377e566b2b701c1b113530c44c1492c2a
Ropaxis <myrhorodskyi.r@gmail.com>
2025-09-07 21:40:10 +0200

    Adding alias lib

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,6 +1,6 @@
 import Logo from "@/app/svg/Logo"
 import LanguageSwitcher from '@/app/LanguageSwitcher';
-import Like from '@/app/Like/index';
+import Like from '@/app/Like/Like';
 
 export default function PageHeader() {
     return (
